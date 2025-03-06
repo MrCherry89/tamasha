e75254017a89ae21756f44fe488b299acc34d8a8
MrCherry89 <abgarnati@yandex.ru>
2025-03-07 01:16:56 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -45,6 +45,51 @@ $(document).ready(function () {
     speed: 700,
   });
 
+  $(".slider-for4").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    initialSlide: 1,
+    asNavFor: ".slider-nav4",
+    prevArrow: $(".planning-solutions .slider-navigation .slick-prev"),
+    nextArrow: $(".planning-solutions .slider-navigation .slick-next"),
+  });
+  $(".slider-nav4").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for4",
+    dots: false,
+    arrows: false,
+    focusOnSelect: true,
+    vertical: true,
+    initialSlide: 1, // Синхронизация со вторым слайдом
+    centerMode: true,
+    centerPadding: 0,
+    responsive: [
+      {
+        breakpoint: 1101,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          vertical: false,
+          slidesToShow: 2,
+          focusOnSelect: false,
+          autoplay: true,
+        },
+      },
+    ],
+  });
+
 
   AOS.init();
 });
